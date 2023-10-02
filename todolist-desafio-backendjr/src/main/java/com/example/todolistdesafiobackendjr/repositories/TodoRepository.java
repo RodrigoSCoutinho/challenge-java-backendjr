ec9cb3592fbd79d7b01b99c04afed12e65cef2f8
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-01 22:52:29 -0300

    feat: implemented rules of the todo-service

---
+++ todolist-desafio-backendjr/src/main/java/com/example/todolistdesafiobackendjr/repositories/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.example.todolistdesafiobackendjr.repositories;
+
+import com.example.todolistdesafiobackendjr.entities.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+}
---
+++ todolist-desafio-backendjr/src/main/java/com/example/todolistdesafiobackendjr/services/TodoService.java
@@ -0,0 +1,41 @@
+package com.example.todolistdesafiobackendjr.services;
+
+import java.util.List;
+
+import com.example.todolistdesafiobackendjr.entities.Todo;
+import com.example.todolistdesafiobackendjr.repositories.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Direction.DESC, "prioridade")
+                .and(Sort.by(Direction.ASC, "id"));
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}

03ea33aa144b05476c52fc447a18852ff6c154bf
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-01 22:27:57 -0300

    feat: implemented entitiy

---
+++ todolist-desafio-backendjr/src/main/java/com/example/todolistdesafiobackendjr/entities/Todo.java
@@ -0,0 +1,56 @@
+package com.example.todolistdesafiobackendjr.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean done;
+    private int priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}

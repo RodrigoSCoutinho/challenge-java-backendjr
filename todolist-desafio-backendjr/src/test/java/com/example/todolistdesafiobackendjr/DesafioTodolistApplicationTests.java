4f717c71f6b78f4d5128711fb7d3fe7ee45a7415
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-01 22:18:51 -0300

    Initial commit

---
+++ todolist-desafio-backendjr/src/test/java/com/example/todolistdesafiobackendjr/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistdesafiobackendjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
